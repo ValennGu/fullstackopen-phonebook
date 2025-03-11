f8329dc9cebf5a821442663bcebd10f86525a83f
Valentin <valentin.g.l@gmail.com>
2025-03-11 09:07:00 +0100

    submit part3.a 3.7 - 3.8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 let persons = [
   { 
@@ -25,7 +26,12 @@ let persons = [
   }
 ]
 
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
