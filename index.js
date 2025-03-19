7df0960e947c8cc29e9dfc5420c38380d3b139e9
Valentin <valentin.g.l@gmail.com>
2025-03-19 08:40:39 +0100

    add server static rendering

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ morgan.token('body', function (req, res) {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
