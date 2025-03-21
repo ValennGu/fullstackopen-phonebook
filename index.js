28aeb374238ec6a47995d3a583fe68c3f173f0b3
Valentin <valentin.g.l@gmail.com>
2025-03-21 16:05:39 +0100

    extract error handling and undefined endpoints to middleware

--- index.js
+++ index.js
@@ -10,87 +10,90 @@ morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   Person
     .find({})
     .then(result => {
       res.json(result)
     })
+    .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person
-    .findById(req.params.id)
+    .findById({})
     .then(result => {
       if (!result) {
-        res.status(404).send({ error: 'Person not found.'})
+        res.status(404).send({ error: 'Person not found'})
       } else {
         res.send(result)
       }
     })
-    .catch(err => {
-      console.log(err)
-      res.status(400).send('Bad request.')
-    })
+    .catch(err => next(err))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person
     .findByIdAndDelete(req.params.id)
     .then(result => {
       if (!result) {
-        res.status(404).send({ error: 'Person not found and therefore could not delete.' })
+        res.status(404).send({ error: 'Person not found and therefore could not delete' })
       } else {
-        res.send(result)
+        res.status(204).send(result)
       }
     })
-    .catch(err => {
-      console.log(err)
-      res.status(400).send({ error: 'Bad request.' })
-    })
+    .catch(err => next(err))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
 
   if (!body) {
-    res.status(400).send({ error: 'No body has been provided.'})
+    res.status(400).send({ error: 'No body has been provided'})
   } else if (!body.name) {
-    res.status(400).send({ error: 'Mising name.'})
+    res.status(400).send({ error: 'Mising name'})
   } else if (!body.number) {
-    res.status(400).send({ error: 'Mising number.'})
+    res.status(400).send({ error: 'Mising number'})
   } else {
     // TODO: Find if the person already exists in the Database.
-    // const match = persons.find(p => p.number === body.number)
-    
-    // if (match) {
-      // res.status(400).send({ error: 'Number already exists in the phonebook.'})
-    // } else {
-      const person = new Person({
-        name: body.name,
-        number: body.number,
-      })
+    const person = new Person({
+      name: body.name,
+      number: body.number,
+    })
 
-      person
-        .save()
-        .then(result => res.send(result))
-    // } 
+    person
+      .save()
+      .then(result => res.send(result))
+      .catch(err => next(err))
   }
 })
 
 app.get('/info', (req, res) => {
-  const count = 'TODO'
-  const date = new Date()
-
-  res.send(`
-    <p>Phonebook has info for ${count} people.</p> 
-    <p>${date}</p>
-  `)
+  Person
+    .countDocuments()
+    .then(result => {
+      res.send(`
+        <p>Phonebook has info for ${result} people.</p> 
+        <p>${new Date()}</p>
+      `)
+    })
 })
+
+const unkownEndpoint = (req, res) =>
+  res.status(404).send({ error: 'Unkown endpoint' })
+
+const errorHandler = (error, req, res, next) =>
+  res.status(400).send({
+    error: 'Bad request',
+    message: error.message
+  })
+
+app.use(unkownEndpoint)
+app.use(errorHandler)
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`)
